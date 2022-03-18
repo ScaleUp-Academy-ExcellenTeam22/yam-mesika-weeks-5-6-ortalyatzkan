11d6a3d61a96136dec8ce6b978d9c3388ad1e07e
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-03-18 16:10:56 +0200

    change

--- exercise.py
+++ exercise.py
@@ -1,14 +1,44 @@
-import pathlib
-import os
-from os import listdir
-from os.path import join, isfile
 
-#a function that returns the name of all directory that start with deep in the path he get
-def list_dir_start_with_deep(path):
-     return [ file for file in listdir(path) if file.startswith('deep') and os.path.isdir(os.path.join(path, file))]
 
+def join(*lists ,sep='-'):
+    """
+    A function that get a number of lists and
+     returns one list With the values ​​of all the lists with value of sap between the list
+    :param parameters:list
+    :param sep:a value insert between lists
+    :return:One list of all the lists with sap between them
+    """
+    temp=[]
+    if len(lists)== 0:#if no list was passed to the function
+        return None
+    for index in range (len(lists)):
+        temp+=lists[index]
+        if index < len(lists)-1:#If this is not the last list
+            temp+=sep
+    return temp
+
+
+def get_recipe_price(prices,optionals='',**ingredients):
+    """
+    A function that receives a list of products
+    quantities and products that
+    will not be considered in the price and
+    returns the price of the products
+    :param prices:a list of products and prices
+    :param optionals:List of products that will not be included in the price
+    :param ingredients:Quantities of each product
+    :return:the price
+    """
+    final_price=0
+    for product in prices:
+        if product not in optionals:#if the product is not in the list of products that are not included in the price
+            final_price += prices.get(product)*ingredients.get(product)/100
+    return final_price
 
 
 if __name__== '__main__':
-    path='C:\\Users\\user\\Desktop\\ortalYatzkan'
-    print(list_dir_start_with_deep(path))
+    print(join())
+    print(join([1,2],[8],[9,5,6],sep='@'))
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300))
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
+    print(get_recipe_price({}))
