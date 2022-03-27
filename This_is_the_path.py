5d99ddaa70f142d070e116d7ecd6aff571a003ec
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-03-27 22:30:39 +0300

    Corrected errors in This_is_the_path.py

--- This_is_the_path.py
+++ This_is_the_path.py
@@ -3,12 +3,16 @@ from os import listdir
 from os.path import join, isfile
 import pathlib
 
-#a function that returns the name of all directory that start with deep in the path he get
-def list_dir_start_with_deep(path):
-     return [ file for file in listdir(path) if file.startswith('deep') and os.path.isdir(os.path.join(path, file))]
 
+def list_dir_start_with_deep(path:str)->list:
+    """
+    A function that returns the name of all directory that start with deep in the path he get.
+    :param path:Path of Directory.
+    :return:All directory that start with deep in the path he get.
+    """
+    return [file for file in listdir(path) if file.startswith('deep') and os.path.isdir(os.path.join(path, file))]
 
 
 if __name__== '__main__':
-    path = 'C:\\Users\\user\\Desktop\\ortalYatzkan'
-    print(list_dir_start_with_deep(path))
+    path_directory = 'C:\\Users\\user\\Desktop\\ortalYatzkan'
+    print(list_dir_start_with_deep(path_directory))
